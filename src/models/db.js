6ef68aca082665d795188666a5bbaf23c622eafd
ramoncodevale <ramonvalentim88@gmail.com>
2023-08-31 10:33:07 -0300

    add new change db file

--- src/models/db.js
+++ src/models/db.js
@@ -1,9 +1,13 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("coffe", "root", "", {
-    host: "localhost",
-    dialect: "mysql",
-});
+const sequelize = new Sequelize({
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DATABASE,
+    dialect: 'mysql',
+    port: process.env.DB_PORT,
+    host: process.env.DB_HOST,
+})
 
 sequelize.authenticate()
 .then(() => {
