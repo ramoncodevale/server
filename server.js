792ccb059579aa6f65f32af779b6003c78f89c8e
ramoncodevale <ramonvalentim88@gmail.com>
2023-09-01 18:15:39 -0300

    add redis

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import path from "path"
 import cors from "cors";
 import session from "express-session";
+import redis from "redis"
+import connectRedis from "connect-redis"
 
 const app = express();
 
@@ -11,12 +13,18 @@ app.use(cors());
 // Configuração para permitir o uso de imagens
 app.use(express.static(new URL('public', import.meta.url).pathname));
 
+
+const RedisStore = connectRedis(session);
+const redisClient = redis.createClient();
+
 // Rotas
 import loginRoutes from './src/routes/loginRoutes.js';
 
 app.use(express.json());
 app.use(loginRoutes);
+
 app.use(session({
+  store: new RedisStore({ client: redisClient }),
   secret: 'abdajd', 
   resave: false,
   saveUninitialized: true,
