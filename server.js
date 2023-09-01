849882de388c824197cdf2c8d10c324126b91791
ramoncodevale <ramonvalentim88@gmail.com>
2023-09-01 18:22:58 -0300

    new change

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import path from "path"
-import cors from "cors";
-import session from "express-session";
-import redis from "redis"
-import connectRedis from "connect-redis"
+import path from 'path';
+import cors from 'cors';
+import session from 'express-session';
+import redis from 'redis';
+import connectRedis from 'connect-redis';
 
 const app = express();
 
@@ -13,16 +13,17 @@ app.use(cors());
 // Configuração para permitir o uso de imagens
 app.use(express.static(new URL('public', import.meta.url).pathname));
 
-
 const RedisStore = connectRedis(session);
 const redisClient = redis.createClient();
 
-app.use(session({
-  store: new RedisStore({ client: redisClient }),
-  secret: 'abdajd', 
-  resave: false,
-  saveUninitialized: true,
-}));
+app.use(
+  session({
+    store: new RedisStore({ client: redisClient }),
+    secret: 'abdajd',
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 // Rotas
 import loginRoutes from './src/routes/loginRoutes.js';
@@ -30,13 +31,12 @@ import loginRoutes from './src/routes/loginRoutes.js';
 app.use(express.json());
 app.use(loginRoutes);
 
-
 app.get('/', async (req, res) => {
-  res.send("Deu certo")
+  res.send('Deu certo');
 });
 
 const PORT = 8080;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port: ${PORT} http://localhost:${PORT}`)
-})
+  console.log(`Server running on port: ${PORT} http://localhost:${PORT}`);
+});
