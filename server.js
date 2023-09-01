19f4549f12bc080dc16a6bfe7f0f7e8932b0372b
ramoncodevale <ramonvalentim88@gmail.com>
2023-09-01 18:30:47 -0300

    add new store

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ import express from 'express';
 import path from 'path';
 import cors from 'cors';
 import session from 'express-session';
-import redis from 'redis';
-import connectRedis from 'connect-redis';
 
 const app = express();
 
@@ -13,12 +11,10 @@ app.use(cors());
 // Configuração para permitir o uso de imagens
 app.use(express.static(new URL('public', import.meta.url).pathname));
 
-const RedisStore = connectRedis(session);
-const redisClient = redis.createClient();
 
 app.use(
   session({
-    store: new RedisStore({ client: redisClient }),
+    store: new MySQLStore({ createDatabaseTable: true }, db),
     secret: 'abdajd',
     resave: false,
     saveUninitialized: true,
