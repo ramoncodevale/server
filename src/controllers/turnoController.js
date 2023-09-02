af72cbb824b533bc623e84e0c4f117a79111cfac
ramoncodevale <ramonvalentim88@gmail.com>
2023-09-02 16:41:42 -0300

    add new route

--- src/controllers/turnoController.js
+++ src/controllers/turnoController.js
@@ -2,6 +2,25 @@ import Shift from '../models/Shift.js';
 
 import User from '../models/User.js';
 
+export const listShift = async (req,res) => {
+    try {
+    const turno = await Shift.findAll({})
+    return res.json({
+        error: false,
+        message: 'Turno listado',
+        data: turno,
+    });
+    } 
+ catch (error) {
+    console.error('Erro ao listar o turno', error);
+    return res.status(500).json({
+        error: true,
+        message: 'Erro ao listar o turno',
+    });
+}
+}
+
+
 export const createShift = async (req, res) => {
     const { periodo, maquina, ge, metaPorHora, planejado, produzido, desperdicoCafe, desperdicoEmbalagem, qualidade, she } = req.body;
 
--- src/routes/turnoRoutes.js
+++ src/routes/turnoRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express';
 
-import { createShift } from '../controllers/turnoController.js';
+import { listShift, createShift } from '../controllers/turnoController.js';
 import { isAuthenticated } from '../middleware/authMiddleware.js';
 
 const router = express();
 
-router.post('/turno', isAuthenticated, createShift)
+
+router.get('/listar-turno', isAuthenticated, listShift)
+router.post('/cadastrar-turno', isAuthenticated, createShift)
 
 
 export default router;
