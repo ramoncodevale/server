5c71090c7663f993c97d31e89dbd3fbd80a1e88b
ramoncodevale <ramonvalentim88@gmail.com>
2023-09-03 20:58:42 -0300

    add new change

--- src/controllers/turnoController.js
+++ src/controllers/turnoController.js
@@ -1,32 +1,18 @@
-import Shift from '../models/Shift.js';
-import User from '../models/User.js';
-import { authenticateJWT } from '../middleware/authMiddleware.js'; // Importe o middleware de autenticação JWT
-
-export const listShift = async (req, res) => {
-    try {
-        const turno = await Shift.findAll({});
-        return res.json({
-            error: false,
-            message: 'Turno listado',
-            data: turno,
-        });
-    } catch (error) {
-        console.error('Erro ao listar o turno', error);
-        return res.status(500).json({
+export const createShift = async (req, res) => {
+    // Verifique se o campo "usuarioId" está presente no corpo da solicitação
+    if (!req.body.usuarioId) {
+        return res.status(400).json({
             error: true,
-            message: 'Erro ao listar o turno',
+            message: 'Campo "usuarioId" é obrigatório.',
         });
     }
-};
 
-export const createShift = async (req, res) => {
     // Obtenha o ID do usuário autenticado a partir do objeto req.user
     const { usuarioId, operador, periodo, horario, maquina, ge, metaPorHora, planejado, produzido, desperdicoCafe, desperdicoEmbalagem, qualidade, she } = req.body;
 
     try {
-      
         const newShift = await Shift.create({
-            usuarioId: usuarioId,
+            usuarioId, // Use o valor fornecido no corpo da solicitação
             operador, 
             periodo,
             horario,
@@ -54,4 +40,3 @@ export const createShift = async (req, res) => {
         });
     }
 };
-// Proteja a rota de criação de turno com autenticação JWT
