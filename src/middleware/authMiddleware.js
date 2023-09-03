8d813dc70933a88e7a6b901b2c270f5f18761e3a
ramoncodevale <ramonvalentim88@gmail.com>
2023-09-03 19:47:50 -0300

    add new test

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,20 +1,19 @@
+// middleware/authMiddleware.js
+
 import jwt from 'jsonwebtoken';
 
 export const authenticateJWT = (req, res, next) => {
-    const token = req.header('Authorization');
+  const token = req.header('Authorization'); // Certifique-se de que o token JWT esteja sendo enviado no cabeçalho de autorização
 
-    if (!token) {
-        return res.status(401).json({ error: true, message: 'Token não fornecido.' });
+  if (!token) {
+    return res.status(401).json({ error: 'Token de autenticação não fornecido' });
+  }
+
+  jwt.verify(token, 'suaChaveSecreta', (err, user) => {
+    if (err) {
+      return res.status(403).json({ error: 'Falha na autenticação do token' });
     }
-
-    jwt.verify(token, 'suaChaveSecreta', (err, decoded) => {
-        if (err) {
-            return res.status(403).json({ error: true, message: 'Falha na autenticação do token.' });
-        }
-
-        // O token é válido, você pode acessar os dados do usuário decodificados
-        req.user = decoded;
-
-        next(); // Continue com a próxima função de middleware ou rota
-    });
+    req.user = user; // Defina o req.user com as informações do usuário
+    next();
+  });
 };
