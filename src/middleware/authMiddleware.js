72a9daceebd0ff1d4c3d6b1fe7f0119bda3bf32b
ramoncodevale <ramonvalentim88@gmail.com>
2023-09-03 19:41:41 -0300

    new middleware

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,28 +1,20 @@
 import jwt from 'jsonwebtoken';
 
 export const authenticateJWT = (req, res, next) => {
-  const token = req.header('Authorization');
+    const token = req.header('Authorization');
 
-  if (!token) {
-    return res.status(401).json({ error: true, message: 'Token não fornecido.' });
-  }
-
-  // Verifique se o token começa com 'Bearer ' e remova essa parte
-  const tokenParts = token.split(' ');
-  if (tokenParts.length !== 2 || tokenParts[0] !== 'Bearer') {
-    return res.status(401).json({ error: true, message: 'Token mal formatado.' });
-  }
-
-  const tokenValue = tokenParts[1];
-
-  jwt.verify(tokenValue, 'suaChaveSecreta', (err, decoded) => {
-    if (err) {
-      return res.status(403).json({ error: true, message: 'Falha na autenticação do token.' });
+    if (!token) {
+        return res.status(401).json({ error: true, message: 'Token não fornecido.' });
     }
 
-    // O token é válido, você pode acessar os dados do usuário decodificados
-    req.user = decoded;
+    jwt.verify(token, 'suaChaveSecreta', (err, decoded) => {
+        if (err) {
+            return res.status(403).json({ error: true, message: 'Falha na autenticação do token.' });
+        }
 
-    next(); // Continue com a próxima função de middleware ou rota
-  });
+        // O token é válido, você pode acessar os dados do usuário decodificados
+        req.user = decoded;
+
+        next(); // Continue com a próxima função de middleware ou rota
+    });
 };
