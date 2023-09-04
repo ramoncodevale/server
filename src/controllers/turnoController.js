e09b727ae35800dfced7f45bbeaac2cc3b073851
ramoncodevale <ramonvalentim88@gmail.com>
2023-09-03 21:13:29 -0300

    addd

--- src/controllers/turnoController.js
+++ src/controllers/turnoController.js
@@ -22,12 +22,12 @@ export const listShift = async (req, res) => {
 
 export const createShift = async (req, res) => {
     // Verifique se o campo "usuarioId" está presente no corpo da solicitação
-    if (!req.body.usuarioId) {
-        return res.status(400).json({
-            error: true,
-            message: 'Campo "usuarioId" é obrigatório.',
-        });
-    }
+    // if (!req.body.usuarioId) {
+    //     return res.status(400).json({
+    //         error: true,
+    //         message: 'Campo "usuarioId" é obrigatório.',
+    //     });
+    // }
 
     // Obtenha o ID do usuário autenticado a partir do objeto req.user
     const { operador, periodo, horario, maquina, ge, metaPorHora, planejado, produzido, desperdicoCafe, desperdicoEmbalagem, qualidade, she } = req.body;
