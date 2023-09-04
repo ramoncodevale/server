e00747bf1349e205250c8d977572c736a66e8116
ramoncodevale <ramonvalentim88@gmail.com>
2023-09-04 13:35:34 -0300

    alterando a rota

--- src/routes/loginRoutes.js
+++ src/routes/loginRoutes.js
@@ -4,7 +4,7 @@ import { registerUser, loginUser } from "../controllers/loginController.js"
 const router = express.Router();
 
 // Rota pública que permite o registro de um usuário
-router.post('/register', registerUser);
+router.post('/cadastrar-usuario', registerUser);
 
 // Rota pública que permite o login de um usuário e gera um token JWT
 router.post('/login', loginUser);
