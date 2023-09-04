cb53e6a2a1eeae8ff571ea225c059bb31207cecc
ramoncodevale <ramonvalentim88@gmail.com>
2023-09-04 17:32:12 -0300

    add new route

--- src/routes/turnoRoutes.js
+++ src/routes/turnoRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 
-// import { listarRegistrosProducao, criarRegistroProducao } from '../controllers/turnoController.js'
+import { listarRegistrosProducao, cadastrarOperador } from '../controllers/turnoController.js'
+
 
 const router = express();
 
-
+router.post('/cadastrar/operador', cadastrarOperador)
 
 export default router;
