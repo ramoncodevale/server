74a5e90187848a6c72a4d0ab7cf2e0aa62f88961
ramoncodevale <ramonvalentim88@gmail.com>
2023-09-05 13:31:06 -0300

    add new modified

--- src/controllers/turnoController.js
+++ src/controllers/turnoController.js
@@ -115,8 +115,7 @@ export async function cadastrarProducoes(req, res) {
     });
 
     // Envie uma resposta de sucesso
-    res.status(201).json({ message: "Dados de produção salvos com sucesso" });
-  } catch (error) {
+    res.status(201).json(newProductionData); // Retorna o operador criado com status 201 (Created)  } catch (error) {
     // Trate os erros adequadamente
     console.error(error);
     res.status(500).json({ error: "Erro ao salvar os dados de produção" });
