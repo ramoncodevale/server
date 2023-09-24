8f608580a875257f319f9ea67365a87d2fec787c
ramoncodevale <ramonvalentim88@gmail.com>
2023-09-24 20:40:57 -0300

    add new modified

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get('/', async (req, res) => {
   res.send('Deu certo');
 });
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT} http://localhost:${PORT}`);
